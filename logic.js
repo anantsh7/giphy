26e156b71cf1dfa43e66c0c9ce5be9ed58f53898
Anant Sharma <anantsh7@gmail.com>
2018-09-26 22:02:05 -0400

    some more js

--- logic.js
+++ logic.js
@@ -1,21 +1,26 @@
 var topics = ["Spiderman", "Black Panther", "Superman", "Batman", "Hulk"];
 
 
+
+
 $("#button").on("click", function() {
 
-  // Storing our giphy API URL for a random cat image
-  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+  var superhero = $(this).attr("data-super");
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  superhero + "&api_key=QNvIp0k6HQGGpz9SckyLMeqaAXtx6sZj&limit=10";
 
   // Perfoming an AJAX GET request to our queryURL
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    console.log(response);
     var imageUrl = response.data.image_original_url;
-    var catImage = $("<img>");
-      catImage.attr("src", imageUrl);
-      catImage.attr("alt", "cat image");
-      $("#images").prepend(catImage);
+    var superImage = $("<img>");
+      superImage.attr("src", imageUrl);
+      superImage.attr("alt", "superhero image");
+      $("#gifs-view").append(superImage);
     });
 });
 
@@ -25,18 +30,10 @@ function renderButtons() {
 
   $("#button-view").empty();
   for (var i = 0; i < topics.length; i++) {
-    var a = $("<button>");
-    a.addClass("btn btn-md btn-danger btn-block");
-    a.attr("data-name", topics[i]);
-    a.append(topics[i]);
+    var newbutton = $("<button>");
+    newbutton.addClass("btn btn-md btn-danger btn-block");
+    newbutton.attr("data-name", topics[i]);
+    newbutton.append(topics[i]);
     $("#button-view").append(a);
   }
 };
-
-
-$("#button").on("click", function (event) {
-  event.preventDefault();
-  var superhero = $("#nameInput").val().trim();
-  topics.push(superhero);
-  renderButtons();
-});
